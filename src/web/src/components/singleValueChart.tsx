75b4125621ccb578337463dd6a7a5bbd32b67547
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2024-08-24 22:49:45 -0300

    feat: add conditional ibutg color

--- src/web/src/components/singleValueChart.tsx
+++ src/web/src/components/singleValueChart.tsx
@@ -5,9 +5,23 @@ interface SingleValueChartProps {
   value: number;
 }
 
+const ibutgMax = 23.2;
+
 export const SingleValueChart: React.FC<SingleValueChartProps> = ({ title, value }) => {
+    
+    const isIbutgExceeded = value >= ibutgMax;
+    let containerClassName;
+
+    if (title === 'IBUTG') {
+        containerClassName = isIbutgExceeded
+        ? 'p-4 border rounded-lg shadow-md bg-red-400 text-black'
+        : 'p-4 border rounded-lg shadow-md bg-green-400 text-black';
+    } else {
+        containerClassName = 'p-4 border rounded-lg shadow-md bg-white text-black';
+    }
+
   return (
-    <div className="p-4 border rounded-lg shadow-md bg-white text-black">
+    <div className={containerClassName}>
       <h2 className="text-lg font-bold">{title}</h2>
       <div className="text-4xl font-bold">
         {value !== null ? `${value.toFixed(2)}` : 'Loading...'}
